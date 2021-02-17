07043daf2328a7afcd9eecaa85d23b77d078e016
LGXprod <mattghanno@gmail.com>
2021-02-17 13:26:18 +1100

    Created db and progress on generating sample data

---
+++ genSampleData.js
@@ -0,0 +1,65 @@
+const articles = [
+  {
+    title: "Tasmanian and NT Governments hand down 2020-21 Budgets",
+    dateUploaded: "13/11/20",
+    readDuration: 2,
+    tags: JSON.stringify(["WATER"]),
+    content: ""
+  },
+  {
+    title: "Victorian Government alters procurement approach for North East Link",
+    dateUploaded: "13/11/20",
+    readDuration: 4,
+    tags: JSON.stringify(["TAX", "BUDGETS"]),
+    content: ""
+  },
+  {
+    title: "ARTC progresses two major Inland Rail works packages",
+    dateUploaded: "13/11/20",
+    readDuration: 3,
+    tags: JSON.stringify(["ECONOMY"]),
+    content: ""
+  },
+  {
+    title: "Auckland City Hospital project commences",
+    dateUploaded: "03/11/20",
+    readDuration: 2,
+    tags: JSON.stringify([]),
+    content: ""
+  },
+  {
+    title: "Procurement for Coomera Connector Stage One begins",
+    dateUploaded: "17/11/20",
+    readDuration: 2,
+    tags: JSON.stringify([]),
+    content: ""
+  },
+  {
+    title: "NSW Government releases plan for supporting the transition to renewables",
+    dateUploaded: "10/11/20",
+    readDuration: 4,
+    tags: JSON.stringify([]),
+    content: ""
+  },
+  {
+    title: "Infrastructure Victoria releases updated infrastructure strategy",
+    dateUploaded: "17/11/20",
+    readDuration: 3,
+    tags: JSON.stringify([]),
+    content: ""
+  },
+  {
+    title: "Procurement commences for Greater Sydney Bus Region Nine",
+    dateUploaded: "10/11/20",
+    readDuration: 1,
+    tags: JSON.stringify([]),
+    content: ""
+  },
+  {
+    title: "2020-2021 West Australian Budget",
+    dateUploaded: "17/11/20",
+    readDuration: 3,
+    tags: JSON.stringify([]),
+    content: ""
+  }
+];
